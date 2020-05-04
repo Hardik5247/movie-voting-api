147c205e10c5b61080667c092dbaac387ddd25eb
Prateek Sachan <psachan@cs.iitr.ac.in>
2020-05-04 16:12:41 +0530

    Add get all movie and create movie endpoint

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MovieAPIView.as_view())
+    path('', views.MovieCreateView.as_view()),
+    path('all/', views.MovieListView.as_view()),
+    path('<int:pk>/', views.MovieAPIView.as_view())
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from movies.serializers import MovieSerializer
 from .models import Movie
@@ -7,6 +9,30 @@ from .models import Movie
 
 # Create your views here.
 
-class MovieAPIView(GenericAPIView):
-    queryset = Movie
-    serializer_class = MovieSerializer
+class MovieAPIView(APIView):
+
+    def get(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MovieCreateView(APIView):
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieListView(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
